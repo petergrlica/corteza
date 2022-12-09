2a9251c24f8a8ae27e7b719b4358a4e1cdc8dacc
Kelani Tolulope <kelanik8@gmail.com>
2022-12-09 11:47:10 +0100

    Add navigational page block

---
+++ client/web/compose/src/components/PageBlocks/Navigation/Base.vue
@@ -0,0 +1,158 @@
+<template>
+  <wrap
+    :scrollable-body="false"
+    v-bind="$props"
+    v-on="$listeners"
+  >
+    <div class="h-100 w-100 card overflow-hidden">
+      <b-nav
+        :tabs="options.display.appearance === 'tabs'"
+        :pills="options.display.appearance === 'pills'"
+        :small="options.display.appearance === 'small'"
+        :fill="options.display.fillJustify === 'fill'"
+        :justify="options.display.fillJustify === 'justify'"
+        :align="options.display.alignment"
+        class="border-0 h-100"
+      >
+        <b-nav-item
+          v-for="(navItem, index) in options.navigationItems"
+          :key="`navItem-${index}`"
+          :to="toggleToLink(navItem)"
+          :href="toggleHrefLink(navItem)"
+          :disabled="navItem.options.disabled"
+          :style="{ order: index, color: navItem.options.textColor, background: navItem.options.backgroundColor, justifyContent: options.display.alignment }"
+          :link-attrs="{ style: `color: ${navItem.options.textColor}` }"
+          :target="selectTargetOption(navItem.options.item.target)"
+          class="d-flex align-items-center"
+        >
+          <template v-if="navItem.type === 'dropdown' || navItem.options.item.displaySubPages">
+            <b-button
+              :id="`dropdown-popover-${index}`"
+              class="text-decoration-none"
+              variant="link"
+              :style="{ color: navItem.options.textColor, background: navItem.options.backgroundColor }"
+            >
+              {{ displayDropdownText(navItem) }}
+              <span class="ml-1">
+                <font-awesome-icon
+                  :icon="['fas', 'chevron-down']"
+                  size="sm"
+                />
+              </span>
+            </b-button>
+
+            <b-popover
+              ref="dropdown-popover"
+              :target="`dropdown-popover-${index}`"
+              :placement="navItem.options.item.align"
+              delay="0"
+              boundary="window"
+              triggers="click blur"
+            >
+              <template
+                v-if="navItem.type === 'dropdown'"
+              >
+                <div
+                  v-for="(dropdown, dIndex) in navItem.options.item.dropdown.items"
+                  :key="`dropdown-${dIndex}`"
+                >
+                  <a
+                    class="dropdown-item"
+                    :href="dropdown.url"
+                    :disabled="navItem.options.disabled"
+                    :target="selectTargetOption(dropdown.target)"
+                    :style="{ order: dIndex * 2 }"
+                  >
+                    {{ dropdown.label }}
+                  </a>
+
+                  <hr
+                    v-if="dropdown.delimiter"
+                    class="my-1"
+                    :style="{ order: dIndex + 1 }"
+                  >
+                </div>
+              </template>
+
+              <template v-else>
+                <div
+                  v-for="(dropdown, dIndex) in getSubPages(navItem.options.item.pageID)"
+                  :key="`dropdown-${dIndex}`"
+                >
+                  <b-link
+                    class="dropdown-item"
+                    :to="{ name: 'page', params: { pageID: dropdown.pageID } }"
+                    :target="selectTargetOption(dropdown.target)"
+                    :style="{ order: dIndex * 2 }"
+                    :disabled="navItem.options.disabled"
+                  >
+                    {{ dropdown.title }}
+                  </b-link>
+                </div>
+              </template>
+            </b-popover>
+          </template>
+
+          <template v-else-if="!navItem.options.item.displaySubPages">
+            {{ navItem.options.item.label }}
+          </template>
+        </b-nav-item>
+      </b-nav>
+    </div>
+  </wrap>
+</template>
+<script>
+import { mapGetters } from 'vuex'
+import base from '../base'
+
+export default {
+  extends: base,
+
+  computed: {
+    ...mapGetters({
+      pages: 'page/set',
+    }),
+  },
+
+  methods: {
+    getSubPages (selfID) {
+      return this.pages.filter(value => value.selfID === selfID) || []
+    },
+
+    selectTargetOption (target) {
+      switch (target) {
+        case 'sameTab':
+          return '_self'
+        case 'newTab':
+          return '_blank'
+        case 'newWindow':
+          return '_blank'
+      }
+    },
+
+    displayDropdownText (navItem) {
+      if (navItem.type === 'dropdown') {
+        return navItem.options.item.dropdown.label
+      }
+
+      return navItem.options.item.label
+    },
+
+    toggleToLink (navItem) {
+      if (navItem.type === 'dropdown' || navItem.options.item.displaySubPages) {
+        return
+      }
+
+      return navItem.options.item.pageID
+    },
+
+    toggleHrefLink (navItem) {
+      if (navItem.type === 'dropdown' || navItem.options.item.displaySubPages) {
+        return
+      }
+
+      return navItem.options.item.url
+    },
+  },
+}
+</script>
